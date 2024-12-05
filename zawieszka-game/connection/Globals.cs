03b355ec7da0f3f2cfe1576bca6030070217af0e
Mendrejk <bastian.lakomy@gmail.com>
2024-12-05 21:37:30 +0100

    Finish overhauling scenes

--- zawieszka-game/connection/Globals.cs
+++ zawieszka-game/connection/Globals.cs
@@ -1,8 +1,8 @@
-﻿namespace Zawieszka.Connection;
+namespace Zawieszka.Connection;
 
 public static class Globals
 {
-    public const int Port = 7000;
-    public const string DefaultServerIp = "127.0.0.1";
-    public const int MaxConnections = 20;
+	public const int Port = 7000;
+	public const string DefaultServerIp = "127.0.0.1";
+	public const int MaxConnections = 20;
 }
