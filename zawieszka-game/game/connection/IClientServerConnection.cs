1ce1e4b6f78a2ff8c4f1e55d73c4431a9a10b47d
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2024-12-19 21:52:21 +0100

    Add simple interface for comunication between Server and Clients

---
+++ zawieszka-game/game/connection/IClientServerConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Zawieszka.Connection;
+
+public interface IClientServerConnection
+{ 
+    public void SetUsername(string username);
+    public void DisplayNotification(string message);
+    public void DisplayMessage(string message);
+}
