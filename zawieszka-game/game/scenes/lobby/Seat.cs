c3c04df6d111da9b3d7188952f9f60af95de0ca9
Marceli Sokólski <43888534+Pralkopodobny@users.noreply.github.com>
2024-12-27 17:25:20 +0100

    Add not connected Client-side lobby

---
+++ zawieszka-game/game/scenes/lobby/Seat.cs
@@ -0,0 +1,38 @@
+﻿using Godot;
+
+namespace Zawieszka.Scenes.Lobby;
+
+[GlobalClass]
+public partial class Seat : Node
+{
+    private const string DefaultUsernameTxt = "Empty";
+    [Export] private Label UsernameDisplay{ get; set; }
+    [Export] private Label PlayerDisplay { get; set; }
+    [Export] private Button SeatButton { get; set; }
+    [Signal]
+    public delegate void TakeSeatClickEventHandler(int seat);
+    [Export] public int SeatNumber { get; private set; } = 0;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        PlayerDisplay.Text += SeatNumber;
+    }
+
+    public void TakeSeat(string username)
+    {
+        UsernameDisplay.Text = username;
+        SeatButton.Visible = false;
+    }
+
+    public void EmptySeat()
+    {
+        SeatButton.Visible = true;
+        UsernameDisplay.Text = DefaultUsernameTxt;
+    }
+
+    private void _on_seat_button_button_up()
+    {
+        EmitSignal(SignalName.TakeSeatClick, SeatNumber);
+    }
+}
---
+++ zawieszka-game/game/scenes/lobby/SeatsList.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+
+namespace Zawieszka.Scenes.Lobby;
+
+public partial class SeatsList : Node
+{
+    private List<Seat> _seats = [];
+    [Signal]
+    public delegate void RequestTakeSeatEventHandler(int seat);
+    public override void _Ready()
+    {
+        var nani = FindChildren("Seat?", nameof(Seat)); // Seat has to have GlobalClass attribute
+        _seats = nani.Select(x => (Seat)x).OrderBy(x => x.SeatNumber).ToList();
+        foreach (var seat in _seats)
+        {
+            seat.TakeSeatClick += seatNumber => EmitSignal(SignalName.RequestTakeSeat, seatNumber);
+        }
+    }
+
+    public void SetSeat(int seatNumber, string username)
+    {
+        if (username is null)
+        {
+            _seats[seatNumber].EmptySeat();
+        }
+        else
+        {
+            _seats[seatNumber].TakeSeat(username);
+        }
+    }
+}
